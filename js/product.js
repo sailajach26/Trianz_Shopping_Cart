2bfb03fa511e31030f0b39b717972e7caca82d85
Sailaja Chekuri <Sailaja.Chekuri@trianz.com>
2017-03-31 18:28:54 +0530

    initial commit

---
+++ js/product.js
@@ -0,0 +1,10 @@
+//----------------------------------------------------------------
+// product class
+function product(sku, name, description, price, cost) {
+    this.sku = sku; // product code (SKU = stock keeping unit)
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.cost = cost;
+   
+}
